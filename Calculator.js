6e6c44ab45ea5ab139971a4b9b9f0f90348bc97e
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-09-06 18:36:40 -0400

    Selected elements and stored them in variables
    - selected elements of the calculator and stored them in descriptive
    variables

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,18 @@
 "Use strict";
+
+const screen = document.querySelector(".screen");
+const num0 = document.querySelector(".number-0");
+const num1 = document.querySelector(".number-1");
+const num2 = document.querySelector(".number-2");
+const num3 = document.querySelector(".number-3");
+const num4 = document.querySelector(".number-4");
+const num5 = document.querySelector(".number-5");
+const num6 = document.querySelector(".number-6");
+const num7 = document.querySelector(".number-7");
+const num8 = document.querySelector(".number-8");
+const num9 = document.querySelector(".number-9");
+const opAdd = document.querySelector(".operator-add");
+const opSub = document.querySelector(".operator-subtract");
+const opMul = document.querySelector(".operator-multiply");
+const opDiv = document.querySelector(".operator-divide");
+const opEql = document.querySelector(".operator-equal");
